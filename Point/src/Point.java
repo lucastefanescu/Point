b60debee3ff628b98898a4af6157093de6d90a80
poo <poo@Room-pc>
2020-05-21 20:42:00 -0400

    First commit

---
+++ Point/src/Point.java
@@ -0,0 +1,23 @@
+
+
+public class Point {
+	
+	public int x; 
+	public int y; 
+	
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public double obtainXval() {
+		return x;
+	}
+	
+	public double obtainYval() {
+		return y;
+	}
+		
+		
+
+}
